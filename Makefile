18fd013c1e3d70821c5ec6ee353c17fba35a33d5
Aryan Eftekhari <aryan.eftekhari@gmail.com>
2020-07-21 10:33:15 +0200

    update

--- Makefile
+++ Makefile
@@ -1,19 +1,18 @@
 PWD=$(shell pwd)
 
-CXX:=g++
+CXX?=g++
 CXXFLAGS =-O3  #-O0 -g -fsanitize=address
 
-
 RELEASE_DIR=$(PWD)
+SQUIC_DIR=$(shell echo $$HOME)
 
 all: SQUIC_CMD
 release: $(TARGET)
 
 SQUIC_CMD: 
-	@echo "\033[92m>> Compiling SQUIC_CMD (-lSQUIC expected in /usr/local/lib ) \033[0m"
+	@echo -e "\033[92m>> Compiling SQUIC_CMD (-lSQUIC expected in ${SQUIC_DIR} ) \033[0m"
 	$(shell mkdir -p $(RELEASE_DIR)) # make the folder
-	$(CXX) $(CXXFLAGS) -o $(RELEASE_DIR)/$@ SQUIC_CMD.cpp -lSQUIC # compile & link
-
+	$(CXX) $(CXXFLAGS) -o $(RELEASE_DIR)/$@ SQUIC_CMD.cpp  -static-libgfortran -static-libstdc++ -static-libgcc  -L${SQUIC_DIR} -lSQUIC  -Wl,-rpath=${SQUIC_DIR} #compile & link
 clean: 
-	@echo "\033[92m>> Cleaning \033[0m"	
+	@echo -e "\033[92m>> Cleaning \033[0m"	
 	rm -rf  SQUIC_CMD
--- test.m
+++ test.m
@@ -1,70 +1,76 @@
 clc;
 clear all;
 close all
-rng(2);
 
-time_squic=[];
-time_squic_core=[];
-time_quic=[];
+addpath QUIC
 
-err_quic=[];
-err_squic=[];
+
+DATA={};
+DATA.p_set=[];
+
+DATA.time_squic=[];
+DATA.time_squic_core=[];
+DATA.time_quic=[];
+
+DATA.err_l2_squic=[];
+DATA.err_l2_quic=[];
+
+DATA.err_l0_squic=[];
+DATA.err_l0_quic=[];
 
 n=100;
-p_set=100:100:1000;
+DATA.p_set=[20,40,80,160,320,640,1280];
 
-for p=p_set;
+for i=1:length(DATA.p_set);
+
+    p=DATA.p_set(i);
     
-    p
-    
-    density=5/p;
-    r=abs(rand(p,1)+1);
-    iCov= sprandsym(p,density,r);
-    
-    mu=rand(p,1);
-    Y=mvnrnd(mu,inv(iCov),n);
-    
-    % L = lambda_matrix + lambda_scalar*(1-pattern(lambda_matrix))
-    lambda_scalar = 5;
-    lambda_matrix = lambda_scalar/1e12 * double(boolean(iCov));
-    
-    lambda_QUIC                 = lambda_matrix;
-    lambda_QUIC(lambda_QUIC==0) = lambda_scalar;
-    
-    %lambda_full_QUIC = full(lambda_matrix);
-    %lambda_full_QUIC(lambda_full_QUIC==0)=lambda_scalar;
-    
-    max_iter=100;
-    drop_tol=1e-4;
-    term_tol=1e-4;
-    
-    verbose_in=0;
-    
-    disp('SQUIC')
-    tic
-    [X,W,squic_info]=SQUIC(Y,lambda_scalar,lambda_matrix,max_iter,drop_tol,term_tol,verbose_in);
-    time_squic(end+1)=toc
-    
-    time_squic_core(end+1)=squic_info.time_total
-    
-    disp('QUIC')
-    tic
-    [X_QUIC,W_QUIC]=QUICi('default',cov(Y),lambda_QUIC,term_tol, verbose_in, max_iter,eye(p),eye(p));
-    time_quic(end+1)=toc
-    
-    err_squic(end+1)=full(sum(sum(abs(X-iCov))));
-    err_quic(end+1)=sum(sum(abs(X_QUIC-iCov)));
-    
+    for j=1:10
+
+        rng(p+j);
+
+        density=5/p;
+        r=abs(rand(p,1)+1);
+        iCov= sprandsym(p,density,r);
+        
+        mu=rand(p,1);
+        Y=mvnrnd(mu,inv(iCov),n);
+        
+        % L = lambda_matrix + lambda_scalar*(1-pattern(lambda_matrix))
+        lambda_scalar = 5;
+        lambda_matrix = lambda_scalar/1e12 * double(boolean(iCov));
+        
+        lambda_QUIC                 = lambda_matrix;
+        lambda_QUIC(lambda_QUIC==0) = lambda_scalar;
+        
+        %lambda_full_QUIC = full(lambda_matrix);
+        %lambda_full_QUIC(lambda_full_QUIC==0)=lambda_scalar;
+        
+        max_iter=100;
+        drop_tol=1e-4;
+        term_tol=1e-4;
+        
+        verbose_in=0;
+        
+        disp('SQUIC')
+        tic
+        [X,W,squic_info]=SQUIC(Y,lambda_scalar,lambda_matrix,max_iter,drop_tol,term_tol,verbose_in);
+        time_squic(j,i)=toc
+        
+        DATA.time_squic_core(j,i)=squic_info.time_total
+        
+        disp('QUIC')
+        tic
+        [X_QUIC,W_QUIC]=QUIC('default',cov(Y),lambda_QUIC,term_tol, verbose_in, max_iter,eye(p),eye(p));
+        DATA.time_quic(j,i)=toc
+        
+        DATA.err_l2_squic(j,i)=full(sum(sum(abs(X-iCov))));
+        DATA.err_l2_quic(j,i)=sum(sum(abs(X_QUIC-iCov)));
+
+        DATA.err_l0_squic(j,i)=full(sum(sum(abs(boolean(X)-boolean(iCov)))));
+        DATA.err_l0_quic(j,i)=sum(sum(abs(boolean(X_QUIC)-boolean(iCov))));
+    end  
 end
 
-figure
-subplot(1,2,1)
-plot(p_set,err_squic);hold on;
-plot(p_set,err_quic);hold on;
-legend('squic','quic')
+save("results","DATA")
 
-subplot(1,2,2)
-plot(p_set,time_squic);hold on;
-plot(p_set,time_squic_core);hold on;
-plot(p_set,time_quic);hold on;
-legend('squic','squic core','quic')
